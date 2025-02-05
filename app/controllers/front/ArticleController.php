4903d58100a1d5310de36ae79e330a0f3d02fadb
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2025-02-05 11:47:13 +0100

    articlecontroller demarrage

---
+++ app/controllers/front/ArticleController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers\Front;
+
+use App\Core\Controller;
+
+class ArticleController extends Controller {
+    public function show() {
+        
+        echo 'Affichage de l\'article';
+    }
+}
