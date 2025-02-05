8323a2da8825b949ecd13ddfdc1ca4790c465cc2
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2025-02-05 11:48:36 +0100

    dashboradcontroller demarrage

---
+++ app/controllers/back/DashboardController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers\Back;
+
+use App\Core\Controller;
+
+class DashboardController extends Controller {
+    public function index() {
+        // Logique pour afficher le tableau de bord admin
+        echo 'Tableau de bord admin';
+    }
+}
