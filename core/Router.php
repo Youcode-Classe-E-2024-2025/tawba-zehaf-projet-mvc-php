c857f9f462835e0c0f315a3e507816b85c72c71c
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2025-02-05 11:42:17 +0100

    creation de class router

---
+++ core/Router.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Core;
+
+class Router {
+    private $routes = [];
+
+    public function addRoute($method, $path, $handler) {
+        $this->routes[$method][$path] = $handler;
+    }
+
+    public function dispatch($uri) {
+        $method = $_SERVER['REQUEST_METHOD'];
+
+        if (isset($this->routes[$method][$uri])) {
+            return call_user_func($this->routes[$method][$uri]);
+        }
+
+        http_response_code(404);
+        echo "404 Not Found";
+    }
+}
