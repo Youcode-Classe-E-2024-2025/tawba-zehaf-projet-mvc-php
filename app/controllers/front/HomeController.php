140bbdbd975a3d1d1c46f4c3da1882a2ddd90afb
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2025-02-05 16:27:36 +0100

    modification de homecontroller

--- app/controllers/front/HomeController.php
+++ app/controllers/front/HomeController.php
@@ -4,19 +4,26 @@ namespace App\Controllers\Front;
 
 use App\Core\Controller;
 use App\Core\View;
+use App\Models\Article;
 
 class HomeController extends Controller {
     private $view;
+    private $articleModel;
 
     public function __construct() {
         $this->view = new View();
+        $this->articleModel = new Article();
     }
 
     public function index() {
-        // Logique pour afficher la page d'accueil
+        // Récupérer les articles récents
+        $articles = $this->articleModel->findAll();
+
+        // Afficher la vue avec les articles récents
         $this->view->render('front/home.twig', [
             'title' => 'Bienvenue',
-            'message' => 'Bienvenue sur la page d\'accueil'
+            'message' => 'Bienvenue sur la page d\'accueil',
+            'articles' => $articles
         ]);
     }
 }
