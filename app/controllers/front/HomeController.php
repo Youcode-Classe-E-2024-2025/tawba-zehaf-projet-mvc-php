60dcec84c4ff44604ab2f637d741f7e558b7698e
tawbazehaf <115876340+tawbazf@users.noreply.github.com>
2025-02-05 12:10:17 +0100

    modification de class homecontroller en utilisant la class view que jai viens de creer

--- app/controllers/front/HomeController.php
+++ app/controllers/front/HomeController.php
@@ -3,10 +3,20 @@
 namespace App\Controllers\Front;
 
 use App\Core\Controller;
+use App\Core\View;
 
 class HomeController extends Controller {
+    private $view;
+
+    public function __construct() {
+        $this->view = new View();
+    }
+
     public function index() {
-       
-        echo 'Bienvenue sur la page d\'accueil';
+        // Logique pour afficher la page d'accueil
+        $this->view->render('front/home.twig', [
+            'title' => 'Bienvenue',
+            'message' => 'Bienvenue sur la page d\'accueil'
+        ]);
     }
 }
